7c6559f0d5b7fe0fa85c16838d56f58371ada462
Abdulbosit-AiM <abdulbosit.melikuziev@gmail.com>
2022-02-24 03:58:13 +0500

    + btn variable & createNotification()

--- 2.toast-notification/script.js
+++ 2.toast-notification/script.js
@@ -1,1 +1,13 @@
 const btn = document.getElementById('btn')
+
+btn.addEventListener('click', () => {
+    createNotification()
+})
+
+function createNotification() {
+    const notif = document.createElement('div')
+
+    notif.classList.add('toast')
+
+    
+}
