93968d8e8e060c5098ae4905d3c4e65bcf217bf7
Abdulbosit-AiM <abdulbosit.melikuziev@gmail.com>
2022-02-24 04:30:25 +0500

    + style + notfication text + title icon

--- 2.toast-notification/script.js
+++ 2.toast-notification/script.js
@@ -9,11 +9,13 @@ function createNotification() {
     const notif = document.createElement('div')
 
     notif.classList.add('toast')
+    let i = 1
+    notif.innerText = 'New Notification'
+    
 
     container.appendChild(notif)
 
     setTimeout(() => {
         notif.remove()
     }, 3000)
-
 }
