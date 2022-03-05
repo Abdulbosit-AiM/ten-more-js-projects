4ee8db9fd6336c7795de61b5646f45e7b2ec86a7
Abdulbosit-AiM <abdulbosit.melikuziev@gmail.com>
2022-03-06 03:41:22 +0500

    layout + styles + functions + title icon

--- 9.soundboard/script.js
+++ 9.soundboard/script.js
@@ -6,3 +6,26 @@ const sounds = [
     'victory',
     'wrong'
 ]
+
+sounds.forEach((sound) => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+        stopSongs()
+        
+        document.getElementById(sound).play
+    })
+
+    document.body.appendChild(btn)
+})
+
+function stopSongs(){
+    sounds.forEach((sound) => {
+        const song = document.getElementById(sound)
+        song.pause()
+        song.currentTime = 0
+    })
+}
